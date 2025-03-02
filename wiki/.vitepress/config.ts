a04df53e75cf766123f6e4ccdadca949f55ea0a7
LittleRangiferTarandus <694408762@qq.com>
2025-03-02 23:37:44 +0800

    feat: add local search in document site

--- wiki/.vitepress/config.ts
+++ wiki/.vitepress/config.ts
@@ -18,6 +18,47 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/LittleRangiferTarandus/parsnip-kit' }
     ],
     logo: '/logo.svg',
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭搜索'
+                }
+              }
+            }
+          },
+          jp: {
+            translations: {
+              button: {
+                buttonText: '検索',
+                buttonAriaLabel: '検索'
+              },
+              modal: {
+                noResultsText: '関連結果が見つかりません',
+                resetButtonTitle: '検索条件をクリア',
+                footer: {
+                  selectText: '選択',
+                  navigateText: '切り替え',
+                  closeText: '検索を閉じる'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   },
   locales: {
     zh: { label: '中文', ...zh },
